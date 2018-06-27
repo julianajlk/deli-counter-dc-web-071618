82dff36bf8942bfb99e19d854e1b6b12e7f517e3
Juliana Kang <julianajlk@gmail.com>
2018-06-27 23:14:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = []
+
+def line(deli_line)
+  if deli_line == 0
+    puts "The line is currently empty."
+  else
+     items = items + (i+1) +". " + deli_line[i] + ", "
+      puts "The line is currently: ${items}"
+  end
+end
