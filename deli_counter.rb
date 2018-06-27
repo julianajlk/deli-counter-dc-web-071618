a0f8ccac17952eb6da7edc80494a0d8b75a2b340
Juliana Kang <julianajlk@gmail.com>
2018-06-27 23:55:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli > 0
+  if katz_deli != 0
     next_in_line = katz_deli.shift()
     puts "Currently serving #{next_in_line}."
   else
